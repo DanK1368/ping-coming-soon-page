9463599d14808f1e2086d1f0e30c4f179b0b00aa
DanK1368 <danielkoett@gmail.com>
2022-04-28 12:16:20 +0200

    error message added

--- src/components/body/Form.jsx
+++ src/components/body/Form.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import "./Form.scss";
 
 function Form() {
+  const [error, setError] = useState(false);
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const handleChange = e => {
+    if (!emailPattern.test(e.target.value)) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="form">
+    <form className="form" noValidate onSubmit={handleSubmit}>
       <legend className="form__legend">Subscribe and get notified</legend>
       <div className="form__wrapper">
         <label>
           <input
-            className="form__input"
+            formNoValidate
+            className={`form__input ${error ? "invalid" : ""}`}
             type="email"
             placeholder="Your email address"
+            onChange={handleChange}
           />
+          {error && (
+            <span className="error">Please provide a valid email address</span>
+          )}
         </label>
         <button className="form__btn" type="submit">
           Notify Me
